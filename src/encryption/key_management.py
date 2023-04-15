79fd41fb40322f0294d31e47ecfd1234e7b152a6
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2023-04-15 00:40:14 +0000

    refactor duplicate code

---
+++ src/encryption/key_management.py
@@ -0,0 +1,63 @@
+import json
+import os
+import re
+
+def create_or_reuse_master_key():
+    # Define the directory containing the key files
+    key_dir = "keys/"
+
+    # Find the file with the highest numeric suffix
+    key_pattern = re.compile(r"key(\d+)\.json")
+    key_nums = [
+        int(key_pattern.match(fname).group(1))
+        for fname in os.listdir(key_dir)
+        if key_pattern.match(fname)
+    ]
+    max_key_num = max(key_nums) if key_nums else 0
+    max_key_file = os.path.join(key_dir, f"key{max_key_num:03d}.json")
+
+    # Load the contents of the file
+    with open(max_key_file, "r") as f:
+        master_key_data = json.load(f)
+
+    # Check the remaining uses field to determine whether to create a new key or use the existing one
+    if master_key_data["remaining_uses"] == 0:
+        # Create a new key and write it to a new file
+        new_key_id = str(max_key_num + 1).zfill(3)
+        new_key_file = os.path.join(key_dir, f"key{new_key_id}.json")
+        master_key = os.urandom(32)
+        # Generate the new key here...
+        new_key_data = {
+            "key_id": new_key_id,
+            "master_key": list(
+                master_key
+            ),  # Convert binary data to a list for JSON serialization
+            "remaining_uses": 99,
+        }
+        with open(new_key_file, "w") as f:
+            json.dump(new_key_data, f)
+        return master_key, new_key_id
+    else:
+        # Use the existing key
+        master_key = bytes(master_key_data["master_key"])
+        # Decrement the remaining uses field
+        master_key_data["remaining_uses"] -= 1
+        with open(max_key_file, "w") as f:
+            json.dump(master_key_data, f)
+        return master_key, master_key_data["key_id"]
+
+
+def search_master_key(id):
+    key_file = os.path.join("keys/", f"key{id}.json")
+    # Load the contents of the file
+    with open(key_file, "r") as f:
+        master_key_data = json.load(f)
+    master_key = bytes(master_key_data["master_key"])
+    return master_key
+
+
+def generate_key(kms_client, cred):
+    # Generate new key
+    response = kms_client.generate_data_key(
+        KeyId=cred["KEY_ID"], KeySpec="AES_256")
+    return response["Plaintext"]
--- src/server.py
+++ src/server.py
@@ -17,6 +17,8 @@ import re
 from bottle import Bottle, request, response, HTTPError, static_file, redirect
 from werkzeug.utils import secure_filename
 
+from encryption.key_management import create_or_reuse_master_key, generate_key, search_master_key
+
 storage_path: Path = Path(__file__).parent / "storage"
 chunk_path: Path = Path(__file__).parent / "chunk"
 
@@ -46,67 +48,6 @@ session = boto3.Session(
     region_name=cred["REGION_NAME"],
 )
 kms_client = session.client("kms")
-
-
-def create_or_reuse_master_key():
-    # Define the directory containing the key files
-    key_dir = "keys/"
-
-    # Find the file with the highest numeric suffix
-    key_pattern = re.compile(r"key(\d+)\.json")
-    key_nums = [
-        int(key_pattern.match(fname).group(1))
-        for fname in os.listdir(key_dir)
-        if key_pattern.match(fname)
-    ]
-    max_key_num = max(key_nums) if key_nums else 0
-    max_key_file = os.path.join(key_dir, f"key{max_key_num:03d}.json")
-
-    # Load the contents of the file
-    with open(max_key_file, "r") as f:
-        master_key_data = json.load(f)
-
-    # Check the remaining uses field to determine whether to create a new key or use the existing one
-    if master_key_data["remaining_uses"] == 0:
-        # Create a new key and write it to a new file
-        new_key_id = str(max_key_num + 1).zfill(3)
-        new_key_file = os.path.join(key_dir, f"key{new_key_id}.json")
-        master_key = os.urandom(32)
-        # Generate the new key here...
-        new_key_data = {
-            "key_id": new_key_id,
-            "master_key": list(
-                master_key
-            ),  # Convert binary data to a list for JSON serialization
-            "remaining_uses": 99,
-        }
-        with open(new_key_file, "w") as f:
-            json.dump(new_key_data, f)
-        return master_key, new_key_id
-    else:
-        # Use the existing key
-        master_key = bytes(master_key_data["master_key"])
-        # Decrement the remaining uses field
-        master_key_data["remaining_uses"] -= 1
-        with open(max_key_file, "w") as f:
-            json.dump(master_key_data, f)
-        return master_key, master_key_data["key_id"]
-
-
-def search_master_key(id):
-    key_file = os.path.join("keys/", f"key{id}.json")
-    # Load the contents of the file
-    with open(key_file, "r") as f:
-        master_key_data = json.load(f)
-    master_key = bytes(master_key_data["master_key"])
-    return master_key
-
-
-def generate_key():
-    # Generate new key
-    response = kms_client.generate_data_key(
-        KeyId=cred["KEY_ID"], KeySpec="AES_256")
-    return response["Plaintext"]
 
 
 def secure_erase(filename: str, passes=1):
@@ -272,7 +213,7 @@ def upload():
         save_dir.mkdir(exist_ok=True, parents=True)
 
     if mode == "sse":
-        dek_key = generate_key()
+        dek_key = generate_key(kms_client, cred)
         dek_encryptor = AeadEncryptor(dek_key, "chacha")
         metadata = b""
         nonce_dek, encrypted, signature_dek = dek_encryptor.encrypt(
